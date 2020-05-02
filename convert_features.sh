4ff9c538ed82854f88b3179b6e9cde28094626fb
kanishk <kanishk5991@gmail.com>
2020-05-02 20:19:41 +0530

    silly error

---
+++ convert_features.sh
@@ -0,0 +1,1 @@
+python3 script/convert_to_lmdb.py --features_dir ~/Documents/research/vigil/datasets/flickr30k/flickr30k_features --lmdb_file ~/Documents/research/vigil/datasets/flickr30k/flickr30k.lmdb
